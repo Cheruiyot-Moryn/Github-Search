176e99fc0d7401a0645587d900230092b3cd10b5
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-04-17 22:51:34 +0300

    add commit

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Users} from '../class/users';
 
 @Component({
   selector: 'app-users',
@@ -6,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+    
+  users: Users;
+  repositories!: any[];
+  created_at:any;
+  username!: string;
+  
   constructor() { }
 
   ngOnInit(): void {
