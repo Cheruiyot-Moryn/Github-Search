5df0c6d0795234f30100c10b6a6737e95ecc67bf
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-04-18 10:49:29 +0300

    add initial commit

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Repositories} from './repositories.component.spec'
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-repositories',
@@ -7,7 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoriesComponent implements OnInit {
 
-  constructor() { }
+  repositories: Repositories;
+  reponame!: string;
+  show!: number;
+
+  constructor(private dataService: DataService) { 
+    this.repositories = new Repositories ("","","",new Date());
+    this.dataService.getRepoInfo().subscribe((repositories: any) => {
+      console.log(repositories);
+      this.repositories = this.repositories;
+  });
+
+}
+
+
+searchRepo(){
+  this.dataService.updateRepos(this.reponame);
+  this.dataService.getRepoInfo().subscribe((repositories: any) => {
+   console.log(repositories);
+   this.repositories = repositories;
+ });
+ 
+}
+
+loadMore(){
+  this.dataService.updateShow(this.show);
+  this.dataService.getRepoInfo().subscribe((repositories: any) => {
+   console.log(repositories);
+   this.repositories = repositories;
+ });
+ 
+}
+
 
   ngOnInit(): void {
   }
