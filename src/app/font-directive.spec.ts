1d0acf8b26ae2adecf8506749323f9637c54f2a7
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-04-18 09:47:56 +0300

    add modified style

---
+++ src/app/font-directive.spec.ts
@@ -0,0 +1,7 @@
+import { FontDirective } from './font-directive';
+
+describe('FontDirective', () => {
+  it('should create an instance', () => {
+    expect(new FontDirective()).toBeTruthy();
+  });
+});
